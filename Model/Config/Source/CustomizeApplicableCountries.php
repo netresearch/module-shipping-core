af6f9001cca5aa1190fae905c71defabcd2852d6
Christoph Aßmann <christoph.assmann@netresearch.de>
2020-09-09 18:23:39 +0200

    GLS-18: add config source models

---
+++ Model/Config/Source/CustomizeApplicableCountries.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\ShippingCore\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Framework\UrlInterface;
+
+/**
+ * Config source model for the `sallowspecific` setting.
+ */
+class CustomizeApplicableCountries implements OptionSourceInterface
+{
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    public function __construct(UrlInterface $urlBuilder)
+    {
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    public function toOptionArray(): array
+    {
+        $configUrl = $this->urlBuilder->getUrl('adminhtml/system_config/edit', [
+            'section' => 'general',
+            '_fragment' => 'general_country-link',
+        ]);
+
+        return [
+            [
+                'value' => '0',
+                'label' => __('Use countries from <a href="%1">Allowed Countries</a> setting.', $configUrl)
+            ],
+            [
+                'value' => '1',
+                'label' => __('Create a customized country list')
+            ],
+        ];
+    }
+}
