31d69011321f473c0bba7836e00dbc36d9608792
Sebastian Ertner <sebastian.ertner@netresearch.de>
2020-10-27 09:09:59 +0000

    GLS-61: Add cron logic for bulk and cron settings

---
+++ Cron/AutoCreate/OrderCollectionLoader/AutoCreateDisabledException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\ShippingCore\Cron\AutoCreate\OrderCollectionLoader;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class AutoCreateDisabledException extends LocalizedException
+{
+}
--- Model/Config/BatchProcessingConfig.php
+++ Model/Config/BatchProcessingConfig.php
@@ -10,9 +10,12 @@ namespace Netresearch\ShippingCore\Model\Config;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoresConfig;
 
 class BatchProcessingConfig
 {
+    private const CONFIG_PATH_CRON_ENABLED = 'shipping/batch_processing/cron_enabled';
+    private const CONFIG_PATH_CRON_ORDER_STATUS = 'shipping/batch_processing/cron_order_status';
     private const CONFIG_PATH_RETRY_FAILED = 'shipping/batch_processing/retry_failed_shipments';
     private const CONFIG_PATH_NOTIFY_CUSTOMER = 'shipping/batch_processing/autocreate_notify';
 
@@ -21,9 +24,38 @@ class BatchProcessingConfig
      */
     private $scopeConfig;
 
-    public function __construct(ScopeConfigInterface $scopeConfig)
+    /**
+     * @var StoresConfig
+     */
+    private $storesConfig;
+
+    public function __construct(ScopeConfigInterface $scopeConfig, StoresConfig $storesConfig)
     {
         $this->scopeConfig = $scopeConfig;
+        $this->storesConfig = $storesConfig;
+    }
+
+    /**
+     * Obtain the stores which are enabled for cron auto-create.
+     *
+     * @return int[]
+     */
+    public function getAutoCreateStores(): array
+    {
+        $storesConfig = $this->storesConfig->getStoresConfigByPath(self::CONFIG_PATH_CRON_ENABLED);
+        $activeStores = array_filter($storesConfig);
+
+        return array_keys($activeStores);
+    }
+
+    /**
+     * Get allowed order statuses for cron auto-create
+     *
+     * @return string Comma-separated list of order status
+     */
+    public function getAutoCreateOrderStatus(): string
+    {
+        return (string) $this->scopeConfig->getValue(self::CONFIG_PATH_CRON_ORDER_STATUS);
     }
 
     /**
