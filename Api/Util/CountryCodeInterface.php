2e33e6e2f1426fbfc0f6a82baa91c97351abcba5
Christoph Aßmann <christoph.assmann@netresearch.de>
2021-04-26 20:33:59 +0200

    DHLGW-1109: add country code utility

---
+++ Api/Util/CountryCodeInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\ShippingCore\Api\Util;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Utility for retrieving a country's ISO 3166 ALPHA-3 code
+ *
+ * @api
+ */
+interface CountryCodeInterface
+{
+    /**
+     * Obtain three-letter country code from two-letter country code.
+     *
+     * @param string $iso2Code
+     * @return string
+     * @throws NoSuchEntityException
+     */
+    public function getIso3Code(string $iso2Code): string;
+}
---
+++ Model/Util/CountryCode.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\ShippingCore\Model\Util;
+
+use Magento\Directory\Model\Country;
+use Magento\Directory\Model\ResourceModel\Country\Collection;
+use Magento\Directory\Model\ResourceModel\Country\CollectionFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Netresearch\ShippingCore\Api\Util\CountryCodeInterface;
+
+class CountryCode implements CountryCodeInterface
+{
+    /**
+     * @var Collection
+     */
+    private $countryCollection;
+
+    public function __construct(CollectionFactory $countryCollectionFactory)
+    {
+        $this->countryCollection = $countryCollectionFactory->create();
+    }
+
+    public function getIso3Code(string $iso2Code): string
+    {
+        $country = $this->countryCollection->load()->getItemById($iso2Code);
+        if (!$country instanceof Country) {
+            throw new NoSuchEntityException(__('The country code %1 is not available.', $iso2Code));
+        }
+
+        return (string) $country->getData('iso3_code');
+    }
+}
