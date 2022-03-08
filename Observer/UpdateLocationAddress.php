0606534c015cdc2481cb358e3afa49523129fa09
Paul Siedler <paul.siedler@netresearch.de>
2022-03-08 14:22:38 +0100

    DHLGW-1200: set chosen delivery location as order shipping address

---
+++ Observer/UpdateLocationAddress.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Netresearch\ShippingCore\Observer;
+
+use Magento\Directory\Helper\Data;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilderFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Quote\Model\Quote;
+use Magento\Sales\Api\Data\OrderAddressInterface;
+use Magento\Sales\Api\OrderAddressRepositoryInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\ResourceModel\GridInterface;
+use Netresearch\ShippingCore\Api\Data\ShippingSettings\ShippingOption\Selection\AssignedSelectionInterface;
+use Netresearch\ShippingCore\Api\Data\ShippingSettings\ShippingOption\Selection\SelectionInterface;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Codes;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Selection\QuoteSelection;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Selection\QuoteSelectionRepository;
+
+/**
+ * Replace the residential shipping address if a delivery location (e.g. post office) was chosen during checkout.
+ */
+class UpdateLocationAddress implements ObserverInterface
+{
+    /**
+     * @var SearchCriteriaBuilderFactory
+     */
+    private $searchCriteriaBuilderFactory;
+
+    /**
+     * @var FilterBuilder
+     */
+    private $filterBuilder;
+
+    /**
+     * @var QuoteSelectionRepository
+     */
+    private $quoteShippingOptionSelectionRepository;
+
+    /**
+     * @var OrderAddressRepositoryInterface
+     */
+    private $orderAddressRepository;
+
+    /**
+     * @var GridInterface
+     */
+    private $orderGrid;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var Data
+     */
+    private $directoryHelper;
+
+    public function __construct(
+        SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory,
+        FilterBuilder $filterBuilder,
+        QuoteSelectionRepository $quoteShippingOptionSelectionRepository,
+        OrderAddressRepositoryInterface $orderAddressRepository,
+        GridInterface $orderGrid,
+        ScopeConfigInterface $scopeConfig,
+        Data $directoryHelper
+    ) {
+        $this->searchCriteriaBuilderFactory = $searchCriteriaBuilderFactory;
+        $this->filterBuilder = $filterBuilder;
+        $this->quoteShippingOptionSelectionRepository = $quoteShippingOptionSelectionRepository;
+        $this->orderAddressRepository = $orderAddressRepository;
+        $this->orderGrid = $orderGrid;
+        $this->scopeConfig = $scopeConfig;
+        $this->directoryHelper = $directoryHelper;
+    }
+
+    public function execute(Observer $observer): void
+    {
+        /** @var Order $order */
+        $order = $observer->getData('order');
+        /** @var Quote $quote */
+        $quote = $observer->getData('quote');
+
+        if ($order->getIsVirtual()) {
+            // virtual orders are not shipped, no shipping address to update.
+            return;
+        }
+
+        $addressFilter = $this->filterBuilder
+            ->setField(AssignedSelectionInterface::PARENT_ID)
+            ->setValue($quote->getShippingAddress()->getId())
+            ->setConditionType('eq')
+            ->create();
+        $locationFilter = $this->filterBuilder
+            ->setField(SelectionInterface::SHIPPING_OPTION_CODE)
+            ->setValue(Codes::SERVICE_OPTION_DELIVERY_LOCATION)
+            ->setConditionType('eq')
+            ->create();
+
+        $searchCriteriaBuilder = $this->searchCriteriaBuilderFactory->create();
+        $searchCriteria = $searchCriteriaBuilder
+            ->addFilter($addressFilter)
+            ->addFilter($locationFilter)
+            ->create();
+
+        $locationSettings = $this->quoteShippingOptionSelectionRepository->getList($searchCriteria)->getItems();
+
+        if (empty($locationSettings)) {
+            // no delivery location selected during checkout, no need to update shipping address.
+            return;
+        }
+
+        $location = array_reduce(
+            $locationSettings,
+            function (array $carry, QuoteSelection $item) {
+                $carry[$item->getInputCode()] = $item->getInputValue();
+                return $carry;
+            },
+            []
+        );
+
+        /** @var OrderAddressInterface|Order $orderShippingAddress */
+        $orderShippingAddress = $order->getShippingAddress();
+        $orderShippingAddress->setCustomerAddressId(null)
+            ->setStreet($location[Codes::SERVICE_INPUT_DELIVERY_LOCATION_STREET])
+            ->setPostcode($location[Codes::SERVICE_INPUT_DELIVERY_LOCATION_POSTAL_CODE])
+            ->setCity($location[Codes::SERVICE_INPUT_DELIVERY_LOCATION_CITY])
+            ->setCountryId($location[Codes::SERVICE_INPUT_DELIVERY_LOCATION_COUNTRY_CODE])
+            ->setCompany($location[Codes::SERVICE_INPUT_DELIVERY_LOCATION_COMPANY] ?: $location['displayName'] ?: '');
+
+        /**
+         * Delivery locations do not have region/state data. If region is not required for the destination
+         * country, we remove it. Otherwise, we keep the region of the original shipping address, which should match
+         * in most cases.
+         */
+        if (!array_key_exists(
+            $location[Codes::SERVICE_INPUT_DELIVERY_LOCATION_COUNTRY_CODE],
+            $this->directoryHelper->getCountriesWithStatesRequired()
+        )) {
+            $orderShippingAddress->setRegion(null);
+            $orderShippingAddress->setRegionId(null);
+        }
+
+        $this->orderAddressRepository->save($orderShippingAddress);
+
+        if (!$this->scopeConfig->getValue('dev/grid/async_indexing')) {
+            $this->orderGrid->refresh($order->getId());
+        }
+    }
+}
---
+++ Test/Integration/TestCase/Observer/UpdateLocationAddressTest.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Netresearch\ShippingCore\Test\Integration\TestCase\Observer;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use Magento\TestFramework\Helper\Bootstrap;
+use Netresearch\ShippingCore\Api\Data\ShippingSettings\ShippingOption\Selection\AssignedSelectionInterface;
+use Netresearch\ShippingCore\Api\Data\ShippingSettings\ShippingOption\Selection\SelectionInterface;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Codes;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Selection\QuoteSelection;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Selection\QuoteSelectionFactory;
+use Netresearch\ShippingCore\Model\ShippingSettings\ShippingOption\Selection\QuoteSelectionRepository;
+use PHPUnit\Framework\TestCase;
+use TddWizard\Fixtures\Catalog\ProductBuilder;
+use TddWizard\Fixtures\Checkout\CartBuilder;
+use TddWizard\Fixtures\Checkout\CustomerCheckout;
+use TddWizard\Fixtures\Customer\AddressBuilder;
+use TddWizard\Fixtures\Customer\CustomerBuilder;
+use TddWizard\Fixtures\Customer\CustomerFixture;
+use TddWizard\Fixtures\Sales\OrderFixture;
+use TddWizard\Fixtures\Sales\OrderFixtureRollback;
+
+class UpdateLocationAddressTest extends TestCase
+{
+    /**
+     * @var OrderInterface|Order
+     */
+    private static $order;
+
+    private static $shippingOptionInputValues = [
+        Codes::SERVICE_INPUT_DELIVERY_LOCATION_STREET => 'Test Str. 42',
+        Codes::SERVICE_INPUT_DELIVERY_LOCATION_POSTAL_CODE => '04200',
+        Codes::SERVICE_INPUT_DELIVERY_LOCATION_CITY => 'TestCity',
+        Codes::SERVICE_INPUT_DELIVERY_LOCATION_COUNTRY_CODE => 'DE',
+        Codes::SERVICE_INPUT_DELIVERY_LOCATION_COMPANY => 'Testfiliale',
+        'displayName' => 'Packstation 420',
+    ];
+
+    /**
+     * @throws \Exception
+     */
+    public static function createOrderWithServices()
+    {
+        $productBuilders = [];
+        // init simple products
+        for ($i = 0; $i < 3; $i++) {
+            $productBuilders[] = ProductBuilder::aSimpleProduct();
+        }
+
+        // create products
+        $products = array_map(
+            static function (ProductBuilder $productBuilder) {
+                return $productBuilder->build();
+            },
+            $productBuilders
+        );
+
+        //create customer
+        $customerBuilder = CustomerBuilder::aCustomer()
+            ->withAddresses(AddressBuilder::anAddress()->asDefaultBilling()->asDefaultShipping());
+        $customer = $customerBuilder->build();
+        $customerFixture = new CustomerFixture($customer);
+        $customerFixture->login();
+
+        $cartBuilder = CartBuilder::forCurrentSession();
+        foreach ($products as $product) {
+            $qty = 1;
+            $cartBuilder = $cartBuilder->withSimpleProduct($product->getSku(), $qty);
+        }
+
+        // create cart and checkout
+        $cart = $cartBuilder->build();
+        $checkout = CustomerCheckout::fromCart($cart);
+        $checkout = $checkout->withShippingMethodCode('flatrate_flatrate');
+
+        // create delivery location service selection for quote
+        $selectionFactory = Bootstrap::getObjectManager()->create(QuoteSelectionFactory::class);
+        $selectionRepository = Bootstrap::getObjectManager()->create(QuoteSelectionRepository::class);
+        foreach (self::$shippingOptionInputValues as $inputCode => $inputValue) {
+            /** @var QuoteSelection $selection */
+            $selection = $selectionFactory->create();
+            $selection->setData(
+                [
+                    AssignedSelectionInterface::PARENT_ID => $cart->getQuote()->getShippingAddress()->getId(),
+                    SelectionInterface::SHIPPING_OPTION_CODE => Codes::SERVICE_OPTION_DELIVERY_LOCATION,
+                    SelectionInterface::INPUT_CODE => $inputCode,
+                    SelectionInterface::INPUT_VALUE => $inputValue,
+                ]
+            );
+            $selectionRepository->save($selection);
+        }
+
+        self::$order = $checkout->placeOrder();
+
+        $customerFixture->logout();
+    }
+
+    /**
+     * Roll back fixture.
+     *
+     * @throws LocalizedException
+     */
+    public static function createOrderWithServicesRollback()
+    {
+        try {
+            OrderFixtureRollback::create()->execute(new OrderFixture(self::$order));
+        } catch (\Exception $exception) {
+            $argv = $_SERVER['argv'] ?? [];
+            if (in_array('--verbose', $argv, true)) {
+                $message = sprintf("Error during rollback: %s%s", $exception->getMessage(), PHP_EOL);
+                register_shutdown_function('fwrite', STDERR, $message);
+            }
+        }
+    }
+
+
+    /**
+     * @test
+     * @magentoDataFixture createOrderWithServices
+     */
+    public function updateAddress()
+    {
+        $this->assertEquals(
+            self::$shippingOptionInputValues[Codes::SERVICE_INPUT_DELIVERY_LOCATION_STREET],
+            self::$order->getShippingAddress()->getStreetLine(1)
+        );
+        $this->assertEquals(
+            self::$shippingOptionInputValues[Codes::SERVICE_INPUT_DELIVERY_LOCATION_COUNTRY_CODE],
+            self::$order->getShippingAddress()->getCountryId()
+        );
+        $this->assertEquals(
+            self::$shippingOptionInputValues[Codes::SERVICE_INPUT_DELIVERY_LOCATION_POSTAL_CODE],
+            self::$order->getShippingAddress()->getPostcode()
+        );
+        $this->assertEquals(
+            self::$shippingOptionInputValues[Codes::SERVICE_INPUT_DELIVERY_LOCATION_CITY],
+            self::$order->getShippingAddress()->getCity()
+        );
+        $this->assertNull(
+            self::$order->getShippingAddress()->getCustomerAddressId()
+        );
+    }
+}
