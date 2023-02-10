4469d77772ae6a40f34306bc93a89209cb0c6f2b
Christoph Aßmann <christoph.assmann@netresearch.de>
2023-02-10 13:52:43 +0100

    DHLGW-1312: add street splitting post-processing composite rule

---
+++ Model/SplitAddress/SplittingRule/CompositeRule.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\ShippingCore\Model\SplitAddress\SplittingRule;
+
+use Magento\Sales\Api\Data\OrderAddressInterface;
+use Netresearch\ShippingCore\Api\Data\RecipientStreetInterface;
+use Netresearch\ShippingCore\Api\SplitAddress\SplittingRuleInterface;
+
+class CompositeRule implements SplittingRuleInterface
+{
+    /**
+     * @var SplittingRuleInterface[]
+     */
+    private $rules;
+
+    /**
+     * @param SplittingRuleInterface[] $rules
+     */
+    public function __construct(array $rules = [])
+    {
+        $this->rules = $rules;
+    }
+
+    /**
+     * Apply all registered rules.
+     *
+     * @param OrderAddressInterface $address
+     * @param RecipientStreetInterface $recipientStreet
+     * @return void
+     */
+    public function apply(OrderAddressInterface $address, RecipientStreetInterface $recipientStreet): void
+    {
+        foreach ($this->rules as $rule) {
+            $rule->apply($address, $recipientStreet);
+        }
+    }
+}
